1eddc1b85823670f46b5861c37517728a774df8f
dinosusu <93073435+dinosusu@users.noreply.github.com>
2022-09-01 12:55:24 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi3.py",
+    version = "0.1",
+    py_modules = ['agi3'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
